1c93c01f3172b1fff24014c87ae84d5dfc37800a
Adam Whitlock <adam.whitlock@adhocteam.us>
2023-03-01 22:42:02 -0700

    added react-window for virt list rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { useEffect, useContext, useState } from "react";
-import { getAllPokemon, singlePokemonSearch } from "@src/pokemon";
-import CardContext from "@src/CardContext";
-
-import { Header, Controls, PokeCard, Toast } from "@comps";
 import { useLiveQuery } from "dexie-react-hooks";
+import { FixedSizeList } from "react-window";
+
+import { getAllPokemon, singlePokemonSearch } from "@src/pokemon";
 import { db } from "./db";
+import CardContext from "@src/CardContext";
+import { Header, Controls, PokeCard, Toast } from "@comps";
 
 function App() {
   const { pokemonGroup, updatePokemonGroup, toast } = useContext(CardContext);
-
-  const [fetching, setFetching] = useState(false);
-
-  const urlsFromDb = useLiveQuery(() => db.urls.limit(100).toArray(), []);
+  const allPokemon = useLiveQuery(() => db.pokemon.toArray(), []);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,36 +17,38 @@ function App() {
 
       if (allUrls && allUrls?.length > 0) return;
 
-      setFetching(true);
-
       await getAllPokemon();
-
-      setFetching(false);
     };
 
     fetchData();
   }, []);
 
+  const Item = ({ index, style }) => {
+    return (
+      <div className="list-item" style={style}>
+        <PokeCard key={index} singlePokemon={allPokemon[index]} />
+      </div>
+    );
+  };
+
   return (
     <>
-      {fetching ? (
-        <div>Loading app...</div>
-      ) : (
-        <>
-          <Toast message={toast.message} toast={toast} />
-          <Header />
-          <Controls />
-          <main>
-            {urlsFromDb && (
-              <div className="char__container">
-                {urlsFromDb.map((char, index) => {
-                  return <PokeCard key={index} url={char.url} />;
-                })}
-              </div>
-            )}
-          </main>
-        </>
-      )}
+      <Toast message={toast.message} toast={toast} />
+      <Header />
+      <Controls />
+      <main>
+        {allPokemon && (
+          <FixedSizeList
+            className="pokemon-list"
+            height={1000}
+            itemCount={allPokemon.length}
+            itemSize={350}
+            width={1800}
+          >
+            {Item}
+          </FixedSizeList>
+        )}
+      </main>
     </>
   );
 }
--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -2,75 +2,61 @@ import { useState, useEffect, useCallback } from "react";
 import { convertMeasurement, typeIcons } from "../utils";
 import { getSinglePokemon } from "@src/pokemon";
 
-const PokeCard = ({ url }) => {
-  const [singlePokemon, setSinglePokemon] = useState(null);
-
-  useEffect(() => {
-    const fetchSinglePokemon = async () => {
-      const pokemon = await getSinglePokemon(url);
-      setSinglePokemon(pokemon);
-    };
-
-    fetchSinglePokemon();
-  }, []);
-
+const PokeCard = ({ singlePokemon, style }) => {
   return (
-    <>
-      {singlePokemon && (
-        <div className={`char__card bg-${singlePokemon.types[0].type.name}`}>
-          <div className="char__card--inner">
-            <div
-              className={`char__color bg-${singlePokemon.types[0].type.name}`}
-            >
-              <div className="char__id">
-                <p>#{String(singlePokemon.id).padStart(3, "0")}</p>
-              </div>
-              <div className="char__icons">
-                {singlePokemon.types.map((type, index) => {
-                  return (
-                    <i
-                      key={index}
-                      className={`fa-solid ${typeIcons[type.type.name]}`}
-                    ></i>
-                  );
-                })}
-              </div>
-            </div>
-
-            <div className="char__overview">
-              <div className="char__img">
-                <img
-                  src={singlePokemon.sprites.front_default}
-                  alt={singlePokemon.name}
-                />
-              </div>
-
-              <div className="char__stats">
-                <p>xp: {singlePokemon.base_experience}</p>
-                <p>ht: {convertMeasurement(singlePokemon.height, 3.048)}ft</p>
-                <p>wt: {convertMeasurement(singlePokemon.weight, 4.536)}lbs</p>
-              </div>
-            </div>
-
-            <div className="char__details">
-              <h2>{singlePokemon.name}</h2>
-              <div className="char__types">
-                {singlePokemon.types.map((type, index) => {
-                  return (
-                    <p
-                      key={index}
-                      className={`char__type--indicator bg-${type.type.name}`}
-                    >
-                      {type.type.name}
-                    </p>
-                  );
-                })}
-              </div>
-            </div>
+    <div
+      style={style}
+      className={`char__card bg-${singlePokemon.types[0].type.name}`}
+    >
+      <div className="char__card--inner">
+        <div className={`char__color bg-${singlePokemon.types[0].type.name}`}>
+          <div className="char__id">
+            <p>#{String(singlePokemon.id).padStart(3, "0")}</p>
+          </div>
+          <div className="char__icons">
+            {singlePokemon.types.map((type, index) => {
+              return (
+                <i
+                  key={index}
+                  className={`fa-solid ${typeIcons[type.type.name]}`}
+                ></i>
+              );
+            })}
           </div>
         </div>
-      )}
-    </>
+
+        <div className="char__overview">
+          <div className="char__img">
+            <img
+              src={singlePokemon.sprites.front_default}
+              alt={singlePokemon.name}
+            />
+          </div>
+
+          <div className="char__stats">
+            <p>xp: {singlePokemon.base_experience}</p>
+            <p>ht: {convertMeasurement(singlePokemon.height, 3.048)}ft</p>
+            <p>wt: {convertMeasurement(singlePokemon.weight, 4.536)}lbs</p>
+          </div>
+        </div>
+
+        <div className="char__details">
+          <h2>{singlePokemon.name}</h2>
+          <div className="char__types">
+            {singlePokemon.types.map((type, index) => {
+              return (
+                <p
+                  key={index}
+                  className={`char__type--indicator bg-${type.type.name}`}
+                >
+                  {type.type.name}
+                </p>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
